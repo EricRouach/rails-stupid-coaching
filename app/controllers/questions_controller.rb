00e267d997f520c6ff4d57ce563ad0a2013219e1
Eric Rouach <erouach@hotmail.fr>
2017-09-04 11:53:06 +0200

    stupid coaching questions

---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,16 @@
+# questions
+class QuestionsController < ApplicationController
+  def answer
+    @your_message = params[:query]
+    if @your_message == 'I am going to work right now!'
+      @answer = 'Great!'
+    elsif @your_message.include? '?'
+      @answer = 'Silly question, get dressed and go to work!'
+    else
+      @answer = 'I don\'t care, get dressed and go to work!'
+    end
+  end
+
+  def ask
+  end
+end
